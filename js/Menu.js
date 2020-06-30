c95081b037dbb215a398c08121154133bc39917d
dchiapa <diegochiapa@gmail.com>
2020-06-30 17:40:41 -0300

    completar reglas

--- js/Menu.js
+++ js/Menu.js
@@ -17,42 +17,30 @@ class Menu {
   }
   reglas(e) {
     e.preventDefault();
-    console.log('reglas');
-  }
-}
-
-function verReglas(e) {
-  e.preventDefault();
-  cambioMenu();
-  let container = document.createElement('div');
-  container.classList.add('alertContainer', 'reglasContainer');
-  container.innerHTML = `
+    let verReglas = new Alert(`
     <h2>Reglas de juego</h2>
-    <ul>
-      <li>
-        <p class="reglassParrafo">Los jugadores elijen el elemento, sin que el oponente lo vea, y luego se muestran juntos,
+      <ul>
+        <li>
+          <p class="reglassParrafo">Los jugadores elijen el elemento, sin que el oponente lo vea, y luego se muestran juntos,
 de modo que pueda verse el elemento que cada uno ha elegido:</p>
-        <ul>        
-          <li>Piedra: un puño cerrado.</li>
-          <li>Papel: todos los dedos extendidos, con la palma de la mano de lado.</li>
-          <li>Tijera: dedos índice y medio extendidos y separados formando una "V".</li>
-        </ul>
-      </li>
-      <li>
-        <p>El objetivo es vencer al oponente seleccionando el arma que gana, 
+          <ul>        
+            <li>Piedra: un puño cerrado.</li>
+            <li>Papel: todos los dedos extendidos, con la palma de la mano de lado.</li>
+            <li>Tijera: dedos índice y medio extendidos y separados formando una "V".</li>
+          </ul>
+        </li>
+        <li>
+          <p>El objetivo es vencer al oponente seleccionando el arma que gana, 
 según las siguientes reglas:</p>
-        <ul>
-          <li>La piedra aplasta la tijera (Gana la piedra).</li>
-          <li>La tijera corta el papel (Gana la tijera).</li>
-          <li>El papel envuelve la piedra (Gana el papel).</li>
-          <li>En caso de empate (que dos jugadores elijan el mismo elemento), se juega otra vez.</li>
-        </ul>
-      </li>
-    </ul>
-    <span class="alertBtn reglasBtn">Ok</span>
-  `;
-  document.querySelector('body').appendChild(container);
-  container.querySelector('.reglasBtn').addEventListener('click', e => {
-    document.querySelector('body').removeChild(container);
-  });
+          <ul>
+            <li>La piedra aplasta la tijera (Gana la piedra).</li>
+            <li>La tijera corta el papel (Gana la tijera).</li>
+            <li>El papel envuelve la piedra (Gana el papel).</li>
+            <li>En caso de empate (que dos jugadores elijan el mismo elemento), se juega otra vez.</li>
+          </ul>
+        </li>
+      </ul>
+    `, ['alertContainer', 'reglasContainer']);
+    document.querySelector('.alertBtn').addEventListener('click', verReglas.action);
+  }
 }
