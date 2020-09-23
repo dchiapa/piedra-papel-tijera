07b47f3145fd9f5d2ed6eef65bb04ede352983b3
Diego Chiapa <diegochiapa@gmail.com>
2020-09-23 18:41:55 -0300

    mejorar interaccion menu

--- js/menu.js
+++ js/menu.js
@@ -4,8 +4,25 @@ function element(selector) {
 let btnHamburger = element(".header--hamburger");
 let menuContainer = element(".header--menu");
 btnHamburger.addEventListener("click", () => {
-  btnHamburger.classList.toggle("not-active");
-  btnHamburger.classList.toggle("active");
-  menuContainer.classList.toggle("not-active");
-  menuContainer.classList.toggle("active");
+  if (
+    !btnHamburger.classList.contains("active") &&
+    !btnHamburger.classList.contains("not-active")
+  ) {
+    btnHamburger.classList.add("active");
+    menuContainer.classList.add("active");
+    return;
+  }
+  if (btnHamburger.classList.contains("active")) {
+    btnHamburger.classList.remove("active");
+    btnHamburger.classList.add("not-active");
+    menuContainer.classList.remove("active");
+    menuContainer.classList.add("not-active");
+    return;
+  } else {
+    btnHamburger.classList.toggle("not-active");
+    btnHamburger.classList.toggle("active");
+    menuContainer.classList.toggle("not-active");
+    menuContainer.classList.toggle("active");
+    return;
+  }
 });
