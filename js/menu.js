8e830250174a5b9eb9644b6462ec8d66bf9f4315
Diego Chiapa <diegochiapa@gmail.com>
2020-09-23 16:24:49 -0300

    interactividad menu

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,11 @@
-let btnHamburger = document.querySelector(".header--hamburger");
+function element(selector) {
+  return document.querySelector(`${selector}`);
+}
+let btnHamburger = element(".header--hamburger");
+let menuContainer = element(".header--menu");
 btnHamburger.addEventListener("click", () => {
   btnHamburger.classList.toggle("not-active");
   btnHamburger.classList.toggle("active");
+  menuContainer.classList.toggle("not-active");
+  menuContainer.classList.toggle("active");
 });
